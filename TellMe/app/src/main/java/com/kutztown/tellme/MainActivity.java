49996146bf546c0cad3044e52b900a02edfe5625
zkern870 <zkern870@live.kutztown.edu>
2016-02-07 00:14:46 -0500

    Started the screen flow
    created the general format of the screens. Added 3 buttons to the
    mainActivity; Login, Create Account; Login as guest. Login as guest
    takes you to a blank activity which will hold all the feed buttons
    because a guest can view the feeds. The login and Create account buttons
    will get sent to login activities. The create account activity will have
    to modified to to the point where it is Create Account but it is close.
    The formatting for these buttons still have to be worked on. I am also
    in the process of adding a home button to each of the activities that
    will take you back to the mainActivity.

--- TellMe/app/src/main/java/com/kutztown/tellme/MainActivity.java
+++ TellMe/app/src/main/java/com/kutztown/tellme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kutztown.tellme;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-//This is a test comment
+
 public class MainActivity extends AppCompatActivity {
+    /**
+     * This will be called when the Login Guest button is clicked.
+     * will send it to the main activity.
+     * @param view
+     */
+    public void sendMain(View view){
+        Intent intent = new Intent(this, DisplayGuestMain.class);
+        startActivity(intent);
+    }
+
+    /**
+     * This will be called when Login button is clicked.
+     * will send app to Login Activity.
+     * @param view
+     */
+    public void sendLogin(View view){
+        Intent intent1 = new Intent(this, sendLogin.class);
+        startActivity(intent1);
+    }
+
+    /**
+     * This will be called when Create Account button is clicked.
+     * will send to a login Activity
+     * @param view
+     */
+    public void sendCreateAccount(View view){
+        Intent intent2 = new Intent(this, DisplayCreateAccount.class);
+        startActivity(intent2);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
